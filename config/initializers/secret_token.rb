d3aa0f649fff76a9153b2c4c3d5fd486f1ac137f
Joshua Whatley <jjwhatley@hotmail.com>
2013-03-29 19:53:25 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SKED::Application.config.secret_token = '5738e00a08b6ff1780e055f55b76cbde7218e94446270f02bd7e00b5e21ff2871a1fd76dcb33b0919af64d8d1d3cd0b7a690fa778309069cb0c251c0bde5a88e'
